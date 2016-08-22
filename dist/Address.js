b53a747ee90a44d72d8dfb9db400dac40007dc5f
Markus Bohl <markus.bohl@gmail.com>
2016-08-23 00:15:27 +0200

    extends Address with useful fields

--- dist/Address.js
+++ dist/Address.js
@@ -24,9 +24,42 @@ var Domain;
             enumerable: true,
             configurable: true
         });
+        Object.defineProperty(Address.prototype, "street", {
+            get: function () {
+                return this._street;
+            },
+            set: function (street) {
+                this._street = street;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(Address.prototype, "housenumber", {
+            get: function () {
+                return this._housenumber;
+            },
+            set: function (housenumber) {
+                this._housenumber = housenumber;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(Address.prototype, "postcode", {
+            get: function () {
+                return this._postcode;
+            },
+            set: function (postcode) {
+                this._postcode = postcode;
+            },
+            enumerable: true,
+            configurable: true
+        });
         Object.defineProperty(Address.prototype, "city", {
             get: function () {
                 return this._city;
+            },
+            set: function (city) {
+                this._city = city;
             },
             enumerable: true,
             configurable: true
--- src/Address.ts
+++ src/Address.ts
@@ -5,6 +5,10 @@ namespace Domain {
 
         private _id: number;
         private _contact: Contact;
+
+        private _street: string;
+        private _housenumber: string;
+        private _postcode: string;
         private _city: string;
 
         set id(value: number) {
@@ -21,6 +25,34 @@ namespace Domain {
 
         get contact(): Contact {
             return this._contact;
+        }
+
+        set street(street: string) {
+            this._street = street;
+        }
+
+        get street(): string {
+            return this._street;
+        }
+
+        set housenumber(housenumber: string) {
+            this._housenumber = housenumber;
+        }
+
+        get housenumber(): string {
+            return this._housenumber;
+        }
+
+        set postcode(postcode: string) {
+            this._postcode = postcode;
+        }
+
+        get postcode(): string {
+            return this._postcode;
+        }
+
+        set city(city: string) {
+            this._city = city;
         }
 
         get city(): string {
