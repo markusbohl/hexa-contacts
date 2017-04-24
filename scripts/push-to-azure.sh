6306bc10d698fa6305de5990430a77383a8e66ec
Markus Bohl <markus.bohl@gmail.com>
2017-04-25 00:32:11 +0200

    Use Travis Azure-Provider

--- scripts/push-to-azure.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-git remote add azure https://mb-azure-deployment-user:$AZURE_DEPLOYMENT_PASSWORD@mb-hexa-contacts.scm.azurewebsites.net:443/mb-hexa-contacts.git
-git push azure master
