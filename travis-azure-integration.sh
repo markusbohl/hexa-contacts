a676faa11ea2307c319903f0484380ff6fbf8aee
Markus Bohl <markus.bohl@gmail.com>
2017-04-30 22:00:04 +0200

    Test CI

--- travis-azure-integration.sh
+++ travis-azure-integration.sh
@@ -1,27 +1,23 @@
 #!/usr/bin/env bash
 
-echo 'mkdir release'
+$GIT_USER = $1
+$GIT_EMAIL = $2
+$AZURE_USER = $3
+$AZURE_PASSWORD = $4
+$AZURE_SITE = $5
+
 mkdir release
-echo 'cp stuff into release'
 cp -R dist/ release/
 cp .deployment release/
 cp deploy.sh release/
 cp package.json release/
 
-
-echo 'cd release'
 cd release
-echo 'git init'
 git init
-echo 'git add -A'
 git add -A
-echo 'git commit'
-git -c user.name=$GIT_USERNAME -c user.email=$GIT_EMAIL commit -m "Auto-Deployment"
-echo 'git remote add azure'
-git remote add azure https://$AZURE_WA_USERNAME:$AZURE_WA_PASSWORD@$AZURE_WA_SITE.scm.azurewebsites.net/$AZURE_WA_SITE.git
-echo 'git push'
+git -c user.name=$GIT_USER -c user.email=$GIT_EMAIL commit -m "Auto-Deployment: $TRAVIS_BRANCH $TRAVIS_COMMIT"
+git remote add azure https://$AZURE_USER:$AZURE_PASSWORD@$AZURE_SITE.scm.azurewebsites.net/$AZURE_SITE.git
 git push -f -q azure master
 
-echo 'rm release'
 cd ..
 rm -rf release
