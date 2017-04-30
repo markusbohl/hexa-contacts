5b5b0d2217a730fef4907826cab3bd3518952ce2
Markus Bohl <markus.bohl@gmail.com>
2017-04-30 21:52:20 +0200

    Test CI

--- travis-azure-integration.sh
+++ travis-azure-integration.sh
@@ -16,7 +16,7 @@ git init
 echo 'git add -A'
 git add -A
 echo 'git commit'
-git -c user.name=$GIT_USERNAME -c user.email=$GIT_EMAIL commit -m "Auto-Deployment: $TRAVIS_BRANCH $TRAVIS_COMMIT"
+git -c user.name=$GIT_USERNAME -c user.email=$GIT_EMAIL commit -m "Auto-Deployment"
 echo 'git remote add azure'
 git remote add azure https://$AZURE_WA_USERNAME:$AZURE_WA_PASSWORD@$AZURE_WA_SITE.scm.azurewebsites.net/$AZURE_WA_SITE.git
 echo 'git push'
