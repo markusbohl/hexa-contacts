15330d33aae1bb815103e83ec84d5cc6ef7b67f1
Markus Bohl <markus.bohl@gmail.com>
2017-05-14 23:03:23 +0200

    Update fluent-ts-validator

--- src/frameworkLayer/validators/NewContactDataValidator.ts
+++ src/frameworkLayer/validators/NewContactDataValidator.ts
@@ -9,24 +9,26 @@ export class NewContactDataValidator extends AbstractValidator<any> {
 
         this.validateIfAny(data => data.firstName)
             .isString()
-            .when(data => data.firstName != null)
+            .whenNotNull()
             .withFailureMessage('firstName must be of type string');
 
         this.validateIfAny(data => data.lastName)
             .isString()
-            .when(data => data.lastName != null)
+            .whenNotNull()
             .withFailureMessage('lastName must be of type string');
 
         this.validateIfAny(data => data.email)
             .isString()
             .isEmail()
-            .when(data => data.email != null && data.email !== '')
+            .whenNotNull()
+            .when(data => data.email !== '')
             .withFailureMessage('email must be a valid email-address');
 
         this.validateIfAny(data => data.dateOfBirth)
             .isString()
             .matches(/\d{4}-\d{2}-\d{2}/)
-            .when(data => data.dateOfBirth != null && data.dateOfBirth !== '')
+            .whenNotNull()
+            .when(data => data.dateOfBirth !== '')
             .withFailureMessage('dateOfBirth-format must be YYYY-MM-DD');
     }
 }
